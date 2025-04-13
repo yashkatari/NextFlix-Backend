97b454745810fe69472685bf550218465108d3ae
yashkatari <yaswanthkumar89777@gmail.com>
2025-04-13 10:55:37 +0530

    Describe your changes here

--- db/connectdb.js
+++ db/connectdb.js
@@ -1,9 +1,13 @@
+// connectdb.js
 import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/BikeS");
-    console.log("MongoDB Connected...");
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error connecting to MongoDB: ${error.message}`);
     process.exit(1);
